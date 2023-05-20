c625b5a27f7789a009bd6d82fb8517d1147ff694
mka198-cloud <mka198@uit.no>
2023-05-20 20:40:33 +0200

    add new controller

---
+++ weatherApi/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace weatherApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello, Terraform!";
+        }
+    }
+}
